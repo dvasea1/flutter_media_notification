1b5eafa7471a3967b0d885afd3bd7ff342f2e6e5
Vasile Barbaros <vbarbaros.ebs@gmail.com>
2020-04-29 15:26:48 +0300

    hide notification

--- lib/medianotification.dart
+++ lib/medianotification.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 
 class Medianotification {
   static const MethodChannel _channel =
-  const MethodChannel('medianotification');
+      const MethodChannel('medianotification');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
@@ -13,12 +13,15 @@ class Medianotification {
 
   static void show_notification(String title, String subtitle,
       int currentProgress, int totalDuration) async {
-    await _channel.invokeMethod(
-        'show_media_notification', <String, dynamic>{
+    await _channel.invokeMethod('show_media_notification', <String, dynamic>{
       'title': title,
       'subtitle': subtitle,
       'currentProgress': currentProgress,
       'totalDuration': totalDuration,
     });
+  }
+
+  static void hide_notification() async {
+    await _channel.invokeMethod('hide_media_notification');
   }
 }
