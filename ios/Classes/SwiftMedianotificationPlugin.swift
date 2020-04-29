e106c1c7eec5c4973a0ae4e254c6f3371b1d7eb1
Vasile Barbaros <vbarbaros.ebs@gmail.com>
2020-04-29 15:22:52 +0300

    ios notification

--- ios/Classes/SwiftMedianotificationPlugin.swift
+++ ios/Classes/SwiftMedianotificationPlugin.swift
@@ -5,30 +5,39 @@ import AVFoundation
 import MediaPlayer
 
 public class SwiftMedianotificationPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "medianotification", binaryMessenger: registrar.messenger())
-    let instance = SwiftMedianotificationPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "medianotification", binaryMessenger: registrar.messenger())
+        let instance = SwiftMedianotificationPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
     
-    
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-
-    if (call.method == "getPlatformVersion") {
-               result("iOS " + UIDevice.current.systemVersion)
-           }
-           else if (call.method == "showAlertDialogshowAlertDialog") {
-           print("test")
-            setAudio()
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        
+        if (call.method == "getPlatformVersion") {
+            result("iOS " + UIDevice.current.systemVersion)
+        }
+        else if (call.method == "show_media_notification") {
+            startAudioSession()
             setupRemoteTransportControls()
-            setupNowPlayingInfoPanel()
-            //updateInfoPanelOnPlay()
             
-           }
-  }
+            let parsedData = call.arguments as! [String: Any]
+            
+            setupNowPlayingInfoPanel(title: parsedData["title"] as! String, subtitle: parsedData["subtitle"] as! String
+                , currentProgress: parsedData["currentProgress"] as! Int, totalDuration: parsedData["totalDuration"] as! Int)
+        }
+        else if (call.method == "hide_media_notification") {
+           stopAudioSession()
+        }
+        else if (call.method == "play") {
+            startAudioSession()
+            setupRemoteTransportControls()
+        
+        }
+    }
     
-    public func setAudio(){
+    private var nowPlayingInfo = [String : Any]()
+    
+    private func startAudioSession(){
         do {
             let audioSession = AVAudioSession.sharedInstance()
             try audioSession.setCategory(AVAudioSession.Category.playback, options: [])
@@ -36,51 +45,56 @@ public class SwiftMedianotificationPlugin: NSObject, FlutterPlugin {
         } catch _ { }
     }
 
-   private var nowPlayingInfo = [String : Any]()
-   
-private func setupRemoteTransportControls() {
-
+    private func stopAudioSession(){
+        do {
+            let audioSession = AVAudioSession.sharedInstance()
+            try audioSession.setActive(false)
+        } catch _ { }
+    }
+    
+    private func setupRemoteTransportControls() {
+        
         let commandCenter = MPRemoteCommandCenter.shared()
-
+        
         // Add handler for Play Command
         commandCenter.playCommand.addTarget { event in
             return .success
             
         }
-
+        
         // Add handler for Pause Command
         commandCenter.pauseCommand.addTarget { event in
-           return .success
-           
+            return .success
+            
         }
     }
-
-    private func setupNowPlayingInfoPanel() {
-
-        nowPlayingInfo[MPMediaItemPropertyTitle] = "self.title"
-
-        nowPlayingInfo[MPMediaItemPropertyArtist] = "self.subtitle"
-
+    
+    private func setupNowPlayingInfoPanel(title: String = "", subtitle: String = "", currentProgress: Int, totalDuration: Int) {
+        
+        nowPlayingInfo[MPMediaItemPropertyTitle] = title
+        
+        nowPlayingInfo[MPMediaItemPropertyArtist] = subtitle
+        
         if #available(iOS 10.0, *) {
             nowPlayingInfo[MPNowPlayingInfoPropertyIsLiveStream] = false
         }
-
-        nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = 100
-
-        nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = 200
-
+        
+        nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = currentProgress
+        
+        nowPlayingInfo[MPMediaItemPropertyPlaybackDuration] = totalDuration
+        
         nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = 0 // will be set to 1 by onTime callback
         MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
     }
-
-
-   private func updateInfoPanelOnPlay() {
-       
-       self.nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = 100
-       self.nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = 1
-       
-       MPNowPlayingInfoCenter.default().nowPlayingInfo = self.nowPlayingInfo
-   }
-
- 
+    
+    
+    private func updateInfoPanelOnPlay(currentProgress: Int) {
+        
+        self.nowPlayingInfo[MPNowPlayingInfoPropertyElapsedPlaybackTime] = currentProgress
+        self.nowPlayingInfo[MPNowPlayingInfoPropertyPlaybackRate] = 1
+        
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = self.nowPlayingInfo
+    }
+    
+    
 }
--- lib/medianotification.dart
+++ lib/medianotification.dart
@@ -4,14 +4,21 @@ import 'package:flutter/services.dart';
 
 class Medianotification {
   static const MethodChannel _channel =
-      const MethodChannel('medianotification');
+  const MethodChannel('medianotification');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static void get showDialog async {
-    await _channel.invokeMethod('showAlertDialogshowAlertDialog');
+  static void show_notification(String title, String subtitle,
+      int currentProgress, int totalDuration) async {
+    await _channel.invokeMethod(
+        'show_media_notification', <String, dynamic>{
+      'title': title,
+      'subtitle': subtitle,
+      'currentProgress': currentProgress,
+      'totalDuration': totalDuration,
+    });
   }
 }
