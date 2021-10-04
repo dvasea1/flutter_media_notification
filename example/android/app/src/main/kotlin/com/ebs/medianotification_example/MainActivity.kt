4b888a5a48ed0493de19aadbd70e4ccc3cd561d8
Vasile Barbaros <vasile.barbaros@ebs-integrator.com>
2021-10-04 15:54:35 +0300

    v2 android embedding

--- example/android/app/src/main/kotlin/com/ebs/medianotification_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/ebs/medianotification_example/MainActivity.kt
@@ -5,6 +5,8 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
+io.flutter.embedding.android.FlutterActivity
+
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
